8de1186283790b904ceea1e4ae720b873664c5a8
Caroline Verticchio <mcverticchio@gmail.com>
2016-11-14 18:25:08 -0500

    Added if statement

--- Assistive_Shopping/shopping/static/app/scripts/components/login.jsx
+++ Assistive_Shopping/shopping/static/app/scripts/components/login.jsx
@@ -3,51 +3,7 @@ var React = require('react');
 var User= require('../models/user.js').User;
 
 
-var Signup = React.createClass({
-  getInitialState: function(){
-    return{
-      username: '',
-      password: ''
-    };
-  },
-  handleUsernameInput: function(e){
-    this.setState({username: e.target.value})
-  },
-  handlePasswordInput: function(e){
-    this.setState({password: e.target.value})
-  },
-  handleSignUp: function(e){
-    e.preventDefault();
-    var username= this.state.username;
-    var password= this.state.password;
-
-    this.props.signUp(username, password);
-  },
-  render: function(){
-    return(
-      <div className="col-md-6">
-        <h2>Need an Account? Sign Up!</h2>
-        <form onSubmit={this.handleSignUp} id="signup">
-
-          <div className="form-group">
-            <label htmlFor="email">Email address</label>
-            <input onChange={this.handleUsernameInput} value={this.state.username} className="form-control" name="email" id="email" type="email" placeholder="email" />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="password">Password</label>
-            <input onChange={this.handlePasswordInput} value={this.state.password} className="form-control" name="password" id="password" type="password" placeholder="Password Please" />
-          </div>
-
-          <input className="btn btn-primary" type="submit" value="Sign Me Up!" />
-        </form>
-      </div>
-    )
-  }
-});
-
-
-// var Signin = React.createClass({
+// var Signup = React.createClass({
 //   getInitialState: function(){
 //     return{
 //       username: '',
@@ -56,40 +12,84 @@ var Signup = React.createClass({
 //   },
 //   handleUsernameInput: function(e){
 //     this.setState({username: e.target.value})
-//     // console.log(this.state.username);
 //   },
 //   handlePasswordInput: function(e){
 //     this.setState({password: e.target.value})
 //   },
-//   handleSignIn: function(e){
+//   handleSignUp: function(e){
 //     e.preventDefault();
 //     var username= this.state.username;
 //     var password= this.state.password;
-//     console.log("You submitted!")
-//     this.props.signIn(username, password);
-//     this.setState({username: '', password: ''});
+//
+//     this.props.signUp(username, password);
 //   },
 //   render: function(){
 //     return(
-//         <div className="col-md-6">
-//           <h2>Please Login</h2>
-//           <form onSubmit={this.handleSignIn} id="login">
-//             <div className="form-group">
-//               <label htmlFor="email-login">Email address</label>
-//               <input onChange={this.handleUsernameInput} value={this.state.username} className="form-control" name="email" id="email-login" type="email" placeholder="email" />
-//             </div>
+//       <div className="col-md-6">
+//         <h2>Need an Account? Sign Up!</h2>
+//         <form onSubmit={this.handleSignUp} id="signup">
 //
-//             <div className="form-group">
-//               <label htmlFor="password-login">Password</label>
-//               <input onChange={this.handlePasswordInput} value={this.state.password} className="form-control" name="password" id="password-login" type="password" placeholder="Password Please" />
-//             </div>
+//           <div className="form-group">
+//             <label htmlFor="email">Email address</label>
+//             <input onChange={this.handleUsernameInput} value={this.state.username} className="form-control" name="email" id="email" type="email" placeholder="email" />
+//           </div>
 //
-//             <input className="btn btn-primary" type="submit" value="Beam Me Up!" />
-//           </form>
-//         </div>
+//           <div className="form-group">
+//             <label htmlFor="password">Password</label>
+//             <input onChange={this.handlePasswordInput} value={this.state.password} className="form-control" name="password" id="password" type="password" placeholder="Password Please" />
+//           </div>
+//
+//           <input className="btn btn-primary" type="submit" value="Sign Me Up!" />
+//         </form>
+//       </div>
 //     )
 //   }
 // });
+
+
+var SignInForm = React.createClass({
+  getInitialState: function(){
+    return{
+      username: '',
+      password: ''
+    }
+  },
+  handleUsernameInput: function(e){
+    this.setState({username: e.target.value})
+    // console.log(this.state.username);
+  },
+  handlePasswordInput: function(e){
+    this.setState({password: e.target.value})
+  },
+  handleSignIn: function(e){
+    e.preventDefault();
+    var username= this.state.username;
+    var password= this.state.password;
+    // console.log("You submitted!")
+    this.props.signIn(username, password);
+    this.setState({username: '', password: ''});
+  },
+  render: function(){
+    return(
+        <div className="col-md-6">
+          <h2>Please Login</h2>
+          <form onSubmit={this.handleSignIn} id="login">
+            <div className="form-group">
+              <label htmlFor="email-login">Email address</label>
+              <input onChange={this.handleUsernameInput} value={this.state.username} className="form-control" name="email" id="email-login" type="email" placeholder="email" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="password-login">Password</label>
+              <input onChange={this.handlePasswordInput} value={this.state.password} className="form-control" name="password" id="password-login" type="password" placeholder="Password Please" />
+            </div>
+
+            <input className="btn btn-primary" type="submit" value="Beam Me Up!" />
+          </form>
+        </div>
+    )
+  }
+});
 
 
 
@@ -97,13 +97,19 @@ var LoginSignUpContainer = React.createClass({
   getInitialState: function(){
     var user = new User();
     var userData = localStorage.getItem('user');
+
+    if(userData){
+      user.set(JSON.parse(userData));
+      user.auth();
+    }
+
     return {
       user: user
     };
   },
-  signUp: function(userData){
+  signIn: function(userData){
     var self= this;
-    User.signup(userData.username, userData.password, function(user){
+    User.signin(userData.username, userData.password, function(user){
       self.setState({user: user});
     });
   },
@@ -112,9 +118,9 @@ var LoginSignUpContainer = React.createClass({
       <div className="container">
         <div className="row">
           <div className="col-md-12">
-            <h1>Assistive Shopping</h1>
+            <h1>Assistive Shopping {this.state.user.get('token') ? 'Logged In': ''}</h1>
 
-            <Signup signUp={this.signUp}/>
+            <SignInForm signIn={this.signIn}/>
           </div>
         </div>
       </div>
