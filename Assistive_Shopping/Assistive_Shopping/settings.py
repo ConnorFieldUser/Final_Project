f37a34ce47999e9fa16d34b8f04547bc67ce0bf5
ConnorFieldUser <conquest.field@gmail.com>
2016-11-14 12:06:17 -0500

    added restful

--- Assistive_Shopping/Assistive_Shopping/settings.py
+++ Assistive_Shopping/Assistive_Shopping/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'shopping'
+    'shopping',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
