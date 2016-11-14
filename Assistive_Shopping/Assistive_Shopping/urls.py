3733c07c919e111d75d146aa65f7403cfa90bb14
ConnorFieldUser <conquest.field@gmail.com>
2016-11-14 17:16:27 -0500

    added restful endpoint, 405, method not allowed

--- Assistive_Shopping/Assistive_Shopping/urls.py
+++ Assistive_Shopping/Assistive_Shopping/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from rest_framework.authtoken import views
 
 
-from shopping.views import UserCreateView, IndexView
+from shopping.views import UserCreateView, IndexView, UserCreateAPIView
 
 # URLs: create_user/ login/ admin/
 
@@ -27,5 +27,6 @@ urlpatterns = [
     url('^', include('django.contrib.auth.urls')),
     url(r'^$', IndexView.as_view(), name="index_view"),
     url(r'^create_user/$', UserCreateView.as_view(), name="user_create_view"),
-    url(r'^obtain_token/', views.obtain_auth_token, name="obtain_auth_token")
+    url(r'^obtain_token/', views.obtain_auth_token, name="obtain_auth_token"),
+    url(r'^user_create_api/$', UserCreateAPIView.as_view(), name="user_create_api_view"),
 ]
--- Assistive_Shopping/shopping/views.py
+++ Assistive_Shopping/shopping/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-
 from django.contrib.auth.models import User
-
 from django.contrib.auth.forms import UserCreationForm
-
 from django.views.generic import TemplateView
-
 from django.views.generic.edit import CreateView
 
 
+from rest_framework.generics import CreateAPIView
+from shopping.serializers import UserSerializer
+
 # Create your views here.
+
 
 class IndexView(TemplateView):
         template_name = 'index.html'
@@ -19,3 +19,8 @@ class UserCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = "/"
+
+
+class UserCreateAPIView(CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
