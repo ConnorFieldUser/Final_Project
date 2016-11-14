b16dadfbbecd09d0d1f4bb1c2077bb23b57386a0
ConnorFieldUser <conquest.field@gmail.com>
2016-11-14 13:58:19 -0500

    added profile models and user create page

--- Assistive_Shopping/shopping/admin.py
+++ Assistive_Shopping/shopping/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+
+from shopping.models import Account
+
 # Register your models here.
+
+admin.site.register(Account)
--- Assistive_Shopping/shopping/models.py
+++ Assistive_Shopping/shopping/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
 # Create your models here.
+
+
+class Account(models.Model):
+
+    user = models.OneToOneField('auth.User')
+    created = models.DateTimeField(auto_now_add=True)
+
+
+@receiver(post_save, sender='auth.User')
+def create_user_profile(**kwargs):
+    created = kwargs.get('created')
+    instance = kwargs.get('instance')
+    if created:
+        Account.objects.create(user=instance)
