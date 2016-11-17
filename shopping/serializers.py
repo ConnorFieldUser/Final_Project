3a7979a0e37935c88e40e5432e10495fca7f2dcd
ConnorFieldUser <conquest.field@gmail.com>
2016-11-17 10:50:56 -0500

    fixed user create endpoint :D

--- shopping/serializers.py
+++ shopping/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 from django.contrib.auth.models import User
 from shopping.models import Account, Cart, Item
 
@@ -9,19 +10,33 @@ from shopping.models import Account, Cart, Item
 #         fields = '__all__'
 
 
-class UserSerializer(serializers.ModelSerializer):
+class UserSerializer(ModelSerializer):
+
     class Meta:
         model = User
-        fields = ('username', 'password',)
-        write_only_fields = ('password',)
-        read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
+        fields = ('username', 'password')
 
-    def restore_object(self, attrs, instance=None):
-        # call set_password on user object. Without this
-        # the password will be stored in plain text.
-        user = super(UserSerializer, self).restore_object(attrs, instance)
-        user.set_password(attrs['password'])
+    def create(self, validated_data):
+        user = User(**validated_data)
+        # Hash the user's password.
+        user.set_password(validated_data['password'])
+        user.save()
         return user
+
+
+# class UserSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = ('username', 'password',)
+#         write_only_fields = ('password',)
+#         read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
+#
+#     def restore_object(self, attrs, instance=None):
+#         # call set_password on user object. Without this
+#         # the password will be stored in plain text.
+#         user = super(UserSerializer, self).restore_object(attrs, instance)
+#         user.set_password(attrs['password'])
+#         return user
 
 
 class AccountSerializer(serializers.ModelSerializer):
