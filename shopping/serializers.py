fec75f3f2b7762c7179e7c1ffed4f69a6b79b0b1
ConnorFieldUser <conquest.field@gmail.com>
2016-11-17 09:41:01 -0500

    more changes

--- shopping/serializers.py
+++ shopping/serializers.py
@@ -12,7 +12,7 @@ from shopping.models import Account, Cart, Item
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('password', 'first_name', 'last_name', 'email',)
+        fields = ('username', 'password',)
         write_only_fields = ('password',)
         read_only_fields = ('is_staff', 'is_superuser', 'is_active', 'date_joined',)
 
