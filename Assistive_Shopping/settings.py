938c95b9dab35fe411fe95b2504a605a7822b8ce
ConnorFieldUser <conquest.field@gmail.com>
2016-11-22 11:05:59 -0500

    heroku database

--- Assistive_Shopping/settings.py
+++ Assistive_Shopping/settings.py
@@ -76,12 +76,19 @@ WSGI_APPLICATION = 'Assistive_Shopping.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+heroku_database = dj_database_url.config()
+
+if heroku_database:
+    DATABASES['default'] = heroku_database
 
 
 # Password validation
