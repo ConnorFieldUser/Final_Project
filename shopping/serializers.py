06c75297af6e586bc9ecd07eac8bdf32298943f9
ConnorFieldUser <conquest.field@gmail.com>
2016-11-28 13:02:45 -0500

    added price field

--- shopping/serializers.py
+++ shopping/serializers.py
@@ -41,7 +41,7 @@ class CartSerializer(serializers.ModelSerializer):
     cart_items = serializers.SerializerMethodField()
 
     def get_cart_items(self, obj):
-        return obj.cartitem_set.all().values('id', 'item__name', 'item', 'quantity', 'price')
+        return obj.cartitem_set.all().values('id', 'item__name', 'item', 'quantity', 'item__price')
 
     class Meta:
         model = Cart
