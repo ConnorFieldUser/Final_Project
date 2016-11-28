013e95655f664e4f5f2367f4d26621f0953ce364
ConnorFieldUser <conquest.field@gmail.com>
2016-11-28 12:27:03 -0500

    bundle again. I didn't touch anything...

--- shopping/static/app/scripts/components/map.jsx
+++ shopping/static/app/scripts/components/map.jsx
@@ -2,11 +2,7 @@ var React = require('react');
 var Backbone = require('backbone');
 var TemplateContainer = require('../layout/headerTemplate.jsx').TemplateContainer;
 
-require("react-dom/package.json"); // react-dom is a peer dependency
-var GoogleMapLoader = require('google-maps-react').GoogleMapLoader;
-var Marker = require('google-maps-react').Marker;
-var GoogleMap = require('google-maps-react');
-console.log('googlemap', GoogleMap)
+
 
 var MapLocation = React.createClass({
 render: function() {
